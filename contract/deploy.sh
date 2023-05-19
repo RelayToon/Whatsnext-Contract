6dd4bb1f17d0b0582f1c9278f9109c53a449b5bf
Duhoon <eclip6@naver.com>
2023-05-20 05:44:49 +0900

    feat: scripts

--- contract/deploy.sh
+++ contract/deploy.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
+./scripts/build.sh
 
-./build.sh
+LOGFILE="deploy.log"
+if [ ! -e '$LOGFILE' ];then
+  echo ">> Generating deploy.log"
+  touch deploy.log
+fi
+
+date +%Y-%m-%d_%T >> deploy.log
 
 if [ $? -ne 0 ]; then
-  echo ">> Error building contract"
+  echo ">> Error building contract" >> deploy.log
   exit 1
 fi
 
-echo ">> Deploying contract"
+echo ">> Deploying contract" >> deploy.log
 
-# https://docs.near.org/tools/near-cli#near-dev-deploy
-near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/hello_near.wasm
+# If direcotry,"neardev", is persist, all of cotracts is deployed in same account.
+DIRECTORY=neardev
+if [ -d "$DIRECTORY" ];then
+  echo ">> There is neardev \nRemoving Process" >> deploy.log
+  rm -rf neardev
+fi
+
+# near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/hello_near.wasm | tee -a deploy.log
+near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/voteController.wasm | tee -a deploy.log
+# near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/community.wasm | tee -a deploy.log
+# near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/content.wasm | tee -a deploy.log
+
+
+# Deploing for Vote Contract is deprecated.
+# near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/vote.wasm | tee -a deploy.log
+
+echo "================================================" >> deploy.log
